042a6e6ca006b47818638c8d3b1d9930f8735f60
Orel-Viktor <orelviktor93@gmail.com>
2024-04-18 14:41:01 +0300

    second commit

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 import "./style.css";
+
+let swiper = new Swiper(".mySwiper", {
+  loop: true,
+  spaceBetween: 3,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+let swiper2 = new Swiper(".mySwiper2", {
+  loop: true,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
